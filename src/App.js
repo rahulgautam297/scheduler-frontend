9ea891ef7456a77dc6c813429524d3df3577999c
rahul_gautam <rahulgautam297@gmail.com>
2019-10-06 22:51:47 +0530

    comment out server side render code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Scheduler from './components/Scheduler';
+import WebFontLoader from 'webfontloader';
 
-if(typeof(window) !== 'undefined') {
-  // import('WebFontLoader').then((webFontLoader) => {
-  //   WebFontLoader.load({
-  //     google: {
-  //       families: ['Roboto:300,400,500,700', 'Material Icons']
-  //     },
-  //   });
-  // });
-}
+// Code for server side rendering.
+// if(typeof(window) !== 'undefined') {
+//   import('WebFontLoader').then((webFontLoader) => {
+//     WebFontLoader.load({
+//       google: {
+//         families: ['Roboto:300,400,500,700', 'Material Icons']
+//       },
+//     });
+//   });
+// }
 
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons']
+  },
+});
 class App extends Component {
   render() {
     return (
